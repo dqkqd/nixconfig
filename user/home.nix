f960c9b56dc090ebbbe3cd3d8c0d8111913c2634
Khanh Duong <dqkqdlot@gmail.com>
2026-07-19 15:14:51 +0900

    add npmrc

--- user/home.nix
+++ user/home.nix
@@ -114,6 +114,10 @@
   xdg.configFile = {
     "jj/config.toml".source = ./jujutsu.toml;
   };
+  home.file.".npmrc" = {
+    text = "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}\n";
+    force = true;
+  };
   home.sessionVariables = {
     EDITOR = "nvim";
     # fcitx5
