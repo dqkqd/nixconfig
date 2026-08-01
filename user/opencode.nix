579e71f2dbedbf56cc55d81eb7fce254e68edfd9
Khanh Duong <dqkqdlot@gmail.com>
2026-08-01 14:07:00 +0900

    feat: add more plugins, rtk, dcp

--- user/opencode.nix
+++ user/opencode.nix
@@ -1,8 +1,19 @@
 {
+  lib,
   inputs,
   pkgs,
   ...
 }: {
+  home.packages = with pkgs; [
+    rtk
+  ];
+  # https://www.rtk-ai.app/docs/getting-started/supported-agents/#opencode
+  home.activation = {
+    rtkOpencode = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      ${pkgs.rtk}/bin/rtk init --global --opencode
+    '';
+  };
+
   programs.opencode = {
     enable = true;
     package = inputs.opencode.packages.${pkgs.stdenv.hostPlatform.system}.opencode;
@@ -24,7 +35,11 @@
         cheap = "opencode-go/mimo-v2.5";
       };
     in {
-      plugin = ["superpowers@git+https://github.com/obra/superpowers.git"];
+      plugin = [
+        "superpowers@git+https://github.com/obra/superpowers.git"
+        "@simonwjackson/opencode-direnv"
+        "@tarquinen/opencode-dcp@latest"
+      ];
       agent = {
         zen-research = {
           prompt = builtins.readFile ./agents/zen-researcher.md;
