c0c2de91e365fd1fa1785a15cbbe1b5dda3c6a79
Khanh Duong <dqkqdlot@gmail.com>
2026-08-08 13:58:12 +0900

    user: point gh-image at master for bearer-route uploads

--- user/home.nix
+++ user/home.nix
@@ -8,12 +8,12 @@
 }: let
   gh-image = pkgsUnstable.buildGoModule rec {
     pname = "gh-image";
-    version = "1.2.0";
+    version = "d59ac2b";
     src = pkgsUnstable.fetchFromGitHub {
       owner = "drogers0";
       repo = "gh-image";
-      rev = "v${version}";
-      hash = "sha256-It7DivJXX0PrCRTuZr/tFq89OjheMUiyYCMs69y2qsI=";
+      rev = version;
+      hash = "sha256-K1HKrii5BmngqM2x0yxoQYGYARtgshWmueigO2BEPn4=";
     };
     vendorHash = "sha256-TzVyLcfpa3eN9bHQJnuPuGeiOgxYbBurFdKq0EfpJL4=";
   };
